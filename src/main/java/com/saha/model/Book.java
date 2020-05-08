a61bf009f1a6a44b573a648991093a5fc667fc43
Sumit Saha <sumit.saha@appdynamics.com>
2020-05-09 01:59:40 +0530

    minor change

--- src/main/java/com/saha/model/Book.java
+++ src/main/java/com/saha/model/Book.java
@@ -2,7 +2,10 @@ package com.saha.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,16 +14,21 @@ import javax.persistence.Table;
 @Data
 public class Book {
 
-  // ======================================
-  // =             Attributes             =
-  // ======================================
-
   @Id
   private Long id;
 
+  @Column(name = "title")
   private String title;
+
+  @Column(name = "description")
   private String description;
+
+  @Column(name = "unitCost")
   private Float unitCost;
+
+  @Column(name = "isbn")
   private String isbn;
+
+  @Column(name = "nbOfPage")
   private Integer nbOfPage;
 }
